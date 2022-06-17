c2e9a3546c3e1a565e3bf82ae6c99dfc81dc9161
Kai Oguri <hipopopo.kai@gmail.com>
2022-06-17 21:45:24 +0900

    initial commit

---
+++ test/test.py
@@ -0,0 +1,10 @@
+from starlette.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_health_check():
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'OK'}
